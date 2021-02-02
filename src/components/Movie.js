2c8891252006abdcfb0271a4a425e9bb0df2639d
jhonathanmusa <jhonathansamu91@gmail.com>
2021-02-02 18:55:32 -0500

    adding functional components

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,33 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-export class Movie extends Component {
-  static propTypes = {
-    id: PropTypes.string,
-    poster: PropTypes.string,
-    title: PropTypes.string,
-    year: PropTypes.string,
-  };
-
-  render() {
-    const { id, poster, title, year } = this.props;
-
-    return (
-      <div className="card">
-        <div className="card-image">
-          <figure className="image is-4by3">
-            <img src={poster} alt={title} />
-          </figure>
-        </div>
-        <div className="card-content">
-          <div className="media">
-            <div className="media-content">
-              <p className="title is-4">{title}</p>
-              <p className="subtitle is-6">{year}</p>
-            </div>
+export const Movie = ({ id, poster, title, year }) => {
+  return (
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-4by3">
+          <img src={poster} alt={title} />
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="media">
+          <div className="media-content">
+            <p className="title is-4">{title}</p>
+            <p className="subtitle is-6">{year}</p>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+Movie.propTypes = {
+  id: PropTypes.string,
+  poster: PropTypes.string,
+  title: PropTypes.string,
+  year: PropTypes.string,
+};
